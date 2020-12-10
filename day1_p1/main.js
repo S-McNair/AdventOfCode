d15d1641326ff6130846e5f9306fcd208c5225b9
Shane <shane.mcnair@gmail.com>
2020-12-09 19:17:49 -0800

    day1_p1: solved first problem set

--- day1_p1/main.js
+++ day1_p1/main.js
@@ -4,10 +4,15 @@
 const fs = require('fs')
 const text = fs.readFileSync("./input.txt", "utf-8");
 const inputArray = text.split('\n')
-//console.log(`xray: ${inputArray}`);
+const key = 2020
 
 const finalProduct = (inputArray) => {
-    console.log(`finalProduct: SAT`);
+    const finalPair = inputArray.filter((int) => {
+        const glassSlipper = (key - int)
+        if (inputArray.includes(glassSlipper.toString())) return glassSlipper
+    })
+    const finalAns = (finalPair[0] * finalPair[1])
+    console.log(`finalAns: ${finalAns}`)
 }
 
 finalProduct(inputArray)
