b4f5fba0378be763616235b5a5e29dd87dc1abae
Shane <shane.mcnair@gmail.com>
2020-12-09 18:11:53 -0800

    first commit

---
+++ day1_p1/main.js
@@ -0,0 +1,13 @@
+//Advent of Code 2020
+//Day 1 Problem 1: https://adventofcode.com/2020/day/1
+//given an input array find the pair that when added together = 2020 and then mulitply that pair and return the product
+const fs = require('fs')
+const text = fs.readFileSync("./input.txt", "utf-8");
+const inputArray = text.split('\n')
+//console.log(`xray: ${inputArray}`);
+
+const finalProduct = (inputArray) => {
+    console.log(`finalProduct: SAT`);
+}
+
+finalProduct(inputArray)
